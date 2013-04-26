dddd3af2e27495d44804ec4aadb68be3c35074ae
Michael Lucy <mlucy@rethinkdb.com>
2013-04-26 03:22:48 -0700

    1

--- bin/modeline.sh
+++ bin/modeline.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-printf "%s | Brightness: <%s/%s> | %s" \
+printf "%s | Brightness: %s/%s | Volume: %s | %s" \
     "`date`" \
     "`cat /sys/class/backlight/acpi_video0/brightness`" \
     "`cat /sys/class/backlight/acpi_video0/max_brightness`" \
+    "`vset`" \
     "`acpi -b`"
